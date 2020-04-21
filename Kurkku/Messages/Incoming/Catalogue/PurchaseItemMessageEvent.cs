58acf5eed7337b6e64f2b0b6697debe8b13fc1f7
Quackster <quacksterr.15@gmail.com>
2020-04-22 01:04:54 +1000

    Update PurchaseItemMessageEvent.cs

--- Kurkku/Messages/Incoming/Catalogue/PurchaseItemMessageEvent.cs
+++ Kurkku/Messages/Incoming/Catalogue/PurchaseItemMessageEvent.cs
@@ -28,6 +28,37 @@ namespace Kurkku.Messages.Incoming
             Console.WriteLine("extra data: " + extraData);
             Console.WriteLine("amount: " + amount);
 
+            /*
+            private function _SafeStr_12512():void
+            {
+	            var _local_3:int;
+	            var _local_4:int;
+	            this._SafeStr_12508 = new Map();
+	            var k:int = 1;
+	            var _local_2:int;
+	            while (k <= 100) {
+		            _local_3 = this._SafeStr_5446.utils._SafeStr_6452(true, 1, k);
+		            _local_4 = (k - _local_3);
+		            if ((((_local_4 > _local_2)) && ((this._SafeStr_5446.utils._SafeStr_6860.indexOf(k) == -1)))){
+			            this._SafeStr_12508.add(k, _local_4);
+			            _local_2 = _local_4;
+		            };
+		            k++;
+	            };
+            }
+
+            private function _SafeStr_12514(k:TimerEvent):void
+            {
+	            if (this._SafeStr_12510 > 0){
+		            this._SafeStr_12510 = (this._SafeStr_12510 - 0.1);
+		            if (this._SafeStr_12510 < 0){
+			            this._SafeStr_12510 = 0;
+		            };
+		            this._SafeStr_4607.findChildByName("promo_text_effect").blend = this._SafeStr_12510;
+	            };
+            }
+            */
+
             decimal amountToCharge = catalogueItem.Data.PriceCoins * amount;
 
             if (catalogueItem.AllowBulkPurchase && discount != null)
