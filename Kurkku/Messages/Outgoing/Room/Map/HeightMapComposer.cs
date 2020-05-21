c6d2c7824737ce6ced8b43d6a600e6728d3a07b1
Quackster <quacksterr.15@gmail.com>
2020-05-22 00:25:09 +1000

    Update more headers

--- Kurkku/Messages/Outgoing/Room/Map/HeightMapComposer.cs
+++ Kurkku/Messages/Outgoing/Room/Map/HeightMapComposer.cs
@@ -1,17 +1,36 @@
-﻿namespace Kurkku.Messages.Outgoing
+﻿using Kurkku.Game;
+
+namespace Kurkku.Messages.Outgoing
 {
     class HeightMapComposer : IMessageComposer
     {
-        private string heightmap;
+        private RoomModel map;
 
-        public HeightMapComposer(string heightmap)
+        public HeightMapComposer(RoomModel roomModel)
         {
-            this.heightmap = heightmap;
+            this.map = roomModel;
         }
 
         public override void Write()
         {
-            m_Data.Add(heightmap);
+            m_Data.Add(this.map.MapSizeX);
+            m_Data.Add(this.map.MapSizeX * this.map.MapSizeY);
+
+            for (int y = 0; y < this.map.MapSizeY; y++)
+            {
+                for (int x = 0; x < this.map.MapSizeX; x++)
+                {
+                    if (!this.map.IsTile(new Position(x, y)))
+                    {
+                        m_Data.Add((short)-1);
+                    }
+                    else
+                    {
+                        m_Data.Add((short)1);
+                    }
+                }
+
+            }
         }
     }
 }
