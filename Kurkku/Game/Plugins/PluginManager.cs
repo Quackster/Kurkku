b5c18e4f32d62262fb33a2f07ea4d5d00ea4ecc4
Quackster <quacksterr.15@gmail.com>
2020-05-22 20:09:03 +1000

    Revert "Update PluginManager.cs"
    This reverts commit 74ef5194dc19b68d3783fde5abeb1fb0c05700b2.

--- Kurkku/Game/Plugins/PluginManager.cs
+++ Kurkku/Game/Plugins/PluginManager.cs
@@ -43,9 +43,6 @@ namespace Kurkku.Game
         /// </summary>
         public void ReloadPlugins()
         {
-            if (!Directory.Exists(pluginDictionary))
-                Directory.CreateDirectory(pluginDictionary);
-
             bool isReload = plugins != null;
 
             if (plugins != null)
